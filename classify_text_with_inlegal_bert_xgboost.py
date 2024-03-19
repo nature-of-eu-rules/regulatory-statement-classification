d2453eb371f6a27647efeaa1426c0fb844d2dca9
Christiaan Meijer <c.meijer@esciencecenter.nl>
2024-03-19 16:06:54 +0100

    add probabilities to classification output

--- classify_text_with_inlegal_bert_xgboost.py
+++ classify_text_with_inlegal_bert_xgboost.py
@@ -1,6 +1,7 @@
 import argparse
 from pathlib import Path
 
+import numpy as np
 import xgboost
 
 from train_inlegalbert_xgboost import create_features, class_names
@@ -8,7 +9,7 @@ from train_inlegalbert_xgboost import create_features, class_names
 models = {}
 
 
-def classify_texts(texts: list[str], model_path):
+def classify_texts(texts: list[str], model_path, return_logits: bool = False):
     """Classifies every text in a list of texts using the xgboost model stored in model_path.
 
     The xgboost model will be loaded and used to classify the texts. The texts however will first be processed by a
@@ -22,6 +23,8 @@ def classify_texts(texts: list[str], model_path):
         A list of strings of which each needs to be classified.
     model_path
         The path to a stored xgboost model
+    return_logits
+        return the probabilities of the model
 
     Returns
     -------
@@ -36,6 +39,8 @@ def classify_texts(texts: list[str], model_path):
         models[model_path] = model
 
     model = models[model_path]
+    if return_logits:
+        return model.predict_proba(features)
     return model.predict(features)
 
 
@@ -51,5 +56,7 @@ def parse_arguments():
 
 if __name__ == "__main__":
     model_path, text = parse_arguments()
-    classification = classify_texts([text], model_path)
-    print(f'The model classified the text as a {class_names[classification[0]]} statement.')
+    probabilities = classify_texts([text], model_path, return_logits=True)[0]
+    classification = np.argmax(probabilities)
+    print(f'The model classified the text as a {class_names[classification]} statement.'
+          f' ({", ".join([c + ": " + str(p) for p, c in zip(probabilities, class_names)])})')
